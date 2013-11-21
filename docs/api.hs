78a03b76164917967fa1fd16f0c741536397c540
Quildreen "Sorella" Motta <quildreen@gmail.com>
2013-11-20 23:07:30 -0200

    Type classes.

--- docs/api.hs
+++ docs/api.hs
@@ -1,3 +1,28 @@
+-- * Type classes
+
+class Show a where
+  toString :: a -> String
+
+class Eq a where
+  isEqual :: a -> a -> Bool
+
+class Applicative a where
+  of :: a -> Applicative a
+  ap :: (Applicative f) => f (a -> b) -> f a -> f b
+
+class Functor a where
+  map :: Functor a -> (a -> b) -> f b
+
+class Monad a where
+  chain :: Monad a -> (a -> Monad b) -> Monad b
+
+class Recoverable a where
+  orElse :: Recoverable a -> (a -> Recoverable b) -> Recoverable b
+
+
+
+
+
 -- * Common monadic operations
 --
 -- Since a monad may have an asynchronous value, operations that work generically across monads
@@ -45,3 +70,5 @@ reverse :: [a] -> [a]
 -- * Folds
 foldLeft :: (a -> b -> a) -> a -> [b] -> a
 foldRight :: (a -> b -> b) -> b -> [a] -> b
+
+
